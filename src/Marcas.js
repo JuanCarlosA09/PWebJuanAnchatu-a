d2cbfa5b8f360f1aebdc4a3b9797340df080adbf
KEVIN CHICAIZA <stalin12_@hotmail.com>
2024-06-21 03:14:22 -0500

    Modificado

--- src/Marcas.js
+++ src/Marcas.js
@@ -3,9 +3,14 @@ import React from 'react';
 function Marcas() {
     return (
         <div>
-            <h1>Cuarta</h1>
-            <p>bienvenido.</p>
+            <h1>Adidas</h1>
+            <p>Adidas Originals tiene que conformarse con la segunda cuenta más grande. Pero Adidas
+              en su conjunto tiene mucho más: la cuenta de Instagram "Adidasfootball"
+               tiene otros 33,8 millones de seguidores. </p>
+               <img src="https://ams3.digitaloceanspaces.com/graffica/2022/12/Adidas-Logo-1971-1024x576.jpeg" alt="marca de adidas" className="image" />
+        
         </div>
+
     );
 }
 

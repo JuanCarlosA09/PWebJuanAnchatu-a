471b0dba7d7f6fac82b4aedce12a1bd5ef39aba5
KEVIN CHICAIZA <stalin12_@hotmail.com>
2024-06-21 04:00:26 -0500

    Modificado

--- src/Ropa.js
+++ src/Ropa.js
@@ -3,8 +3,10 @@ import React from 'react';
 function Ropa () {
     return (
         <div>
-            <h1>Quinta</h1>
-            <p>This is the Quinta page.</p>
+            <h1>Adidas</h1>
+            <p>Haz seguimiento a tu pedido 
+            ¡Aprovecha hoy! Conoce en adidas tu ropa favorita para hacer de tu estilo algo único. </p>
+            <img src="https://assets.adidas.com/images/w_450,f_auto,q_auto/2a9bff8cb30341c0847bac8200bc5fb3_9366/GN8441_00_plp_laydown.jpg" alt="marca de adidas" className="image" />
         </div>
     );
 }

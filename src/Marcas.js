17a0aaaf0f7dc8c8928bdbc85465e6e3be9b4cc6
KEVIN CHICAIZA <stalin12_@hotmail.com>
2024-06-21 03:35:50 -0500

    Modificado

--- src/Marcas.js
+++ src/Marcas.js
@@ -8,7 +8,16 @@ function Marcas() {
               en su conjunto tiene mucho más: la cuenta de Instagram "Adidasfootball"
                tiene otros 33,8 millones de seguidores. </p>
                <img src="https://ams3.digitaloceanspaces.com/graffica/2022/12/Adidas-Logo-1971-1024x576.jpeg" alt="marca de adidas" className="image" />
-        
+               <h1>Puma</h1>
+               <p>Puma piensa como adolescente, pero con la memoria de un adulto.
+                 La idea, de acuerdo con el académico argentino especialista en marcas,
+                  el Dr. Nicolás Kfuri, es exitosa porque se trató de una estrategia de
+                   construcción de marca que quería evitar “que la marca se mantuviera vacía,
+                    sin significado, sin emociones ni asociaciones positivas en la mente del consumidor”.
+                   La idea no era vender tanto, sino posicionar a la marca como un bien deseado, explica. Se trataba de re-definir la misión de
+                    la empresa y hacer de Puma “la marca deportiva más deseada en el mundo”, lo que tiene como resultado,
+                     según Kfuri, que “todos los miembros de la empresa tienen claro el camino a seguir”.</p>
+                     <img src="https://s.yimg.com/ny/api/res/1.2/hG6DlcJMhUQOCsg5AgcMnw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM2MA--/https://media.zenfs.com/es/valora_628/db82049f4b3dc4dde65dfe20d9d2174d" alt="marca de puma" className="image" />
         </div>
 
     );
